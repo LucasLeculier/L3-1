63c99cbf6b707f7c7e01efbed7890d6808c88580
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-07 22:58:57 +0200

    doc et commentaires ajoutés

--- AtelierL3/Atelier0/Exercice2/main.py
+++ AtelierL3/Atelier0/Exercice2/main.py
@@ -1,6 +1,4 @@
-# Dictionnaire de poids et tarif par type de lettre
-from types import NoneType
-
+# Dictionnaire de poids (en grammes) et tarif par type de lettre
 COURRIER = {
     "Lettre Verte": {
         20: 1.16,
@@ -27,9 +25,20 @@ COURRIER = {
 # Définitions des fonctions
 
 def trouver_affranchissement(type_lettre_choix: str, poids: int) -> float:
+    """Fonction permettant de trouver le taux d'affranchissement du client selon le poids du courrier et de son type passés en paramètres effectifs
+
+        params:
+            type_lettre_choix: Le type de lettre souhaité
+            poids: Le poids de la lettre
+        
+        returns:
+            tarif_affranchissement: Renvoie le tarif d'affranchissement correspondant 
+                                    à la lettre de type_lettre_choix et selon son poids
+    """
 
     tarif_affranchissement = 0   
 
+    # On parcours le dictionnaire pour avoir le type de lettre et leurs tarifs
     for type_lettre, poids_lettre in COURRIER.items():
 
         if type_lettre_choix == type_lettre:
@@ -46,24 +55,46 @@ def trouver_affranchissement(type_lettre_choix: str, poids: int) -> float:
 
     return tarif_affranchissement
 
-def service_poste() -> NoneType:
+def service_poste() -> None:
+    """Procédure demandant à l'utilisateur de rentrer le type de lettre et son poids, affiche le tarif d'affranchissement correspondant
+        uniquement si le type de lettre existe et si le poids est valide
 
+        returns:
+            None
+    
+    """
+
+    choix_lettre = input("Choisissez : (Lettre Verte) (Lettre Prioritaire) (Ecopli) : \n")
+    
     while True:
-        choix_lettre = input("Choisissez : (Lettre Verte) (Lettre Prioritaire) (Ecopli) : \n")
+        
         if choix_lettre not in COURRIER:
             choix_lettre = input("Ce choix n'existe pas, choisissez (Lettre Verte) (Lettre Prioritaire) (Ecopli) : \n")
         else:
             break
-    
+
+    poids_message = "Entrez le poids de votre lettre en grammes : \n"
+
+    # Boucle pour demander à l'utilisateur de renseigner le poids de la lettre, poids devant etre valide
     while True:
+        
+        choix_poids = input(poids_message) 
+        
         try:
-            choix_poids = int(input("Entrez le poids de votre lettre en grammes : \n"))
-            break
+            choix_poids = int(choix_poids) # Le choix du poids doit etre un entier, on tente donc de convertir la chaine de caractere
+                                            # de l'input en entier et on change le message dans le cas d'une exception
+            
+            if choix_poids <= 0: # Le poids doit etre positif supérieur à 0
+                poids_message = "Ce poids est invalide, veuillez entrer un poids valide entier et supérieur à 0 : \n"
+            else:
+                break
+
         except:
-            print("Poids incorrect.\n")
+            poids_message = "Ce poids est invalide, veuillez entrer un poids valide entier et supérieur à 0 : \n"
 
     le_tarif = trouver_affranchissement(choix_lettre, choix_poids)
 
     print(f"Votre tarif est de {le_tarif}€. \n")
 
+# Appel de la fonction du service de poste pour lancer l'execution du programme
 service_poste()
