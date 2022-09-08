3e1fa994ef3d3a5ecc309ed909da6d8f1b12c316
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-08 21:02:07 +0200

    commit

--- AtelierL3/Atelier2/EXERCICE3/main.py
+++ AtelierL3/Atelier2/EXERCICE3/main.py
@@ -2,7 +2,7 @@ LA_LISTE = [-1, 8, -4, 50, 75, -200, 12, 5, -7]
 
 def separer(L: list) -> list:
 
-    nouvelle_list = []
+    nouvelle_liste = [0] * len(L)
 
     actuel_positif = len(L) - 1
     actuel_negatif = 0
@@ -10,13 +10,13 @@ def separer(L: list) -> list:
     for e in L:
 
         if e < 0:
-            nouvelle_list[actuel_negatif] = e
+            nouvelle_liste[actuel_negatif] = e
             actuel_negatif += 1
         else:
-            nouvelle_list[actuel_positif] = e
+            nouvelle_liste[actuel_positif] = e
             actuel_positif -= 1
 
-    return nouvelle_list
+    return nouvelle_liste
 
 print(LA_LISTE)
 print(separer(LA_LISTE))
