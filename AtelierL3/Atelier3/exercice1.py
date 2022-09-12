ea8d52dfb13d2b7e37473462019f00b8f4ce8173
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-12 15:53:27 +0200

    commit ( ͡° ͜ʖ ͡°)

--- AtelierL3/Atelier3/exercice1.py
+++ AtelierL3/Atelier3/exercice1.py
@@ -64,6 +64,7 @@ def is_mail(str_arg: str) -> tuple[int]:
 
     return validity_tuple
 
+"""
 str_variable2test = 'bisgambiglia_paul@univ-corse.fr'
 print(is_mail(str_variable2test)) # doit renvoyer (1,0)
 str_variable2test = 'bisgambiglia_paulOuniv-corse.fr'
@@ -72,3 +73,92 @@ str_variable2test = 'bisgambiglia_paul@univ-corsePOINTfr'
 print(is_mail(str_variable2test)) # doit renvoyer (0,4)
 str_variable2test = '@univ-corse.fr'
 print(is_mail(str_variable2test)) # doit renvoyer (0,1)
+"""
+
+def mots_n_lettres(lst_mots: list[str], n: int) -> list[str]:
+    """Renvoie une liste de mots de lst_mots contenant exactement n lettres
+
+    Args:
+        lst_mots (list[str]): _description_
+        n (int): _description_
+
+    Returns:
+        list[str]: _description_
+    """
+
+    return [mot for mot in lst_mots if len(mot) == n]
+
+def commence_par(mot: str, prefixe: str) -> bool:
+    """Renvoie True si mot commence par prefixe sinon False
+
+    Args:
+        mot (str): _description_
+        prefixe (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+    starts_with = False
+
+    if len(mot) >= len(prefixe):
+
+        if prefixe == mot[0:len(prefixe)]:
+            starts_with = True
+    
+    return starts_with
+
+def finit_par(mot: str, suffixe: str) -> bool:
+    """Renvoie True si mot se termine par suffixe sinon False
+
+    Args:
+        mot (str): _description_
+        suffixe (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+    ends_with = False
+
+    if len(mot) >= len(suffixe):
+
+        if suffixe == mot[-len(suffixe):]:
+            ends_with = True
+    
+    return ends_with
+
+def finissent_par(lst_mot: list[str], suffixe: str) -> list[str]:
+    """Renvoie la liste de tous les mots contenus dans lst_mot qui se terminent
+    par suffixe
+
+    Args:
+        lst_mot (list[str]): _description_
+        suffixe (str): _description_
+
+    Returns:
+        list[str]: _description_
+    """
+
+    for mot in lst_mot:
+
+        
+
+    
+
+lst_mot = ["jouer", "bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour",
+"finir", "aimer"]
+
+def test_exercice1():
+    print()
+
+    print(mots_n_lettres(lst_mot, 7))
+    print("Résultat attendu :", ['bonjour', 'pouvoir', 'abajour'], "\n")
+
+    print(commence_par("pouvoir", "pouv"))
+    print("Résultat attendu :", "True", "\n")
+
+    print(finit_par("pouvoir", "voir"))
+    print("Résultat attendu :", "True", "\n")
+
+test_exercice1()
