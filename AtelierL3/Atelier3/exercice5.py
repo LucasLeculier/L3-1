8f67ef627c095a5705aecc84552ca50fc9c6533e
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-13 16:01:28 +0200

    commit

--- AtelierL3/Atelier3/exercice5.py
+++ AtelierL3/Atelier3/exercice5.py
@@ -39,7 +39,50 @@ def renverse(car: str) -> str:
         "fermant": [')', ']', '}']
     }
 
-    
+    is_ouvrant = ouvrante(car)
+    is_fermant = fermante(car)
+
+    if is_ouvrant:
+
+        for i in range(0, len(lst_car.get('ouvrant'))):
+
+            if lst_car.get('ouvrant')[i] == car:
+
+                caractère = lst_car.get('fermant')[i]
+    elif is_fermant:
+
+        for i in range(0, len(lst_car.get('fermant'))):
+
+            if lst_car.get('fermant')[i] == car:
+
+                caractère = lst_car.get('ouvrant')[i]
+
+    return caractère
+
+def operateur(car: str) -> bool:
+    """Renvoie True si car est un opérateur + ou *
+
+    Args:
+        car (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+    return car in ["+", "*"]
+
+def nombre(car: str) -> bool:
+    """Renvoie True si car est bien un nombre sinon False
+
+    Args:
+        car (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+    return car.isdigit()
+
 
 if __name__ == "__main__":
 
@@ -52,3 +95,17 @@ if __name__ == "__main__":
     print(fermante(")")) # Doit renvoyer True
 
     print()
+
+    print(renverse(")"))
+    print(renverse("["))
+    print(renverse("+"))
+
+    print()
+
+    print(operateur("+"))
+    print(operateur(")"))
+
+    print()
+
+    print(nombre("456"))
+    print(nombre("("))
