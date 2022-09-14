7d71ca6bf31bdb926ffb81aaaf3aef188c8ea059
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-14 09:36:23 +0200

    commit

---
+++ AtelierL3/Atelier4/exercice2.py
@@ -0,0 +1,12 @@
+def melange_liste(lst: list) -> list:
+    """Renvoie une liste issue du mélange de la liste
+    passée en paramètre
+
+    Args:
+        lst (list): _description_
+
+    Returns:
+        list: _description_
+    """
+
+    
