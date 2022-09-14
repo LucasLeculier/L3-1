ccfe5475615c039ba9aae190d9c14ed391fb72ac
tbmutisma <112128403+tbmutisma@users.noreply.github.com>
2022-09-14 09:51:16 +0200

    commit

--- AtelierL3/Atelier4/exercice2.py
+++ AtelierL3/Atelier4/exercice2.py
@@ -1,6 +1,7 @@
 def melange_liste(lst: list) -> list:
     """Renvoie une liste issue du mélange de la liste
-    passée en paramètre
+    passée en paramètre, la liste passée en paramètre doit
+    etre intacte à la sortie de la fonction
 
     Args:
         lst (list): _description_
@@ -9,4 +10,14 @@ def melange_liste(lst: list) -> list:
         list: _description_
     """
 
-    
+    liste_melangee = [e for e in lst]
+
+    return liste_melangee
+
+if __name__ == '__main__':
+
+    la_liste = [1, 2]
+    print(melange_liste(la_liste))
+    print(la_liste)
+
+ 
